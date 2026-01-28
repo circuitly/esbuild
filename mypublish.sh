2f24dc0c30f0978e1db2d8f3d85c28c98b44849c
Joe Eagar <joe@circuit.ly>
2026-01-28 14:23:59 -0800

    publish to linux-ppc64 too

--- mypublish.sh
+++ mypublish.sh
@@ -4,6 +4,7 @@ make platform-all
 make --no-print-directory publish-win32-x64
 make --no-print-directory publish-linux-x64
 make --no-print-directory publish-linux-arm64
+make --no-print-directory publish-linux-ppc64
 make --no-print-directory publish-win32-arm64
 make --no-print-directory publish-neutral
 make --no-print-directory publish-wasm
